acefbf2b86feff32f8f075da23884c83a009ebac
Sid.be <sid.be@outlook.com>
2023-05-13 13:50:27 +0200

    ajout d'helmet

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const bookRoutes=require('./routes/book.js');
 const userRoutes=require('./routes/auth.js');
 const coverRoute=require('./routes/cover.js');
+const helmet = require("helmet")
 const path = require('path');
 const cors=require('cors');
 const express=require('express');
@@ -10,6 +11,7 @@ const app=express();
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
 const imagesPath = path.join(__dirname, 'images');
 app.use('/image', express.static(imagesPath));
 app.use('/api/book', bookRoutes);
